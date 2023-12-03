5989bb57c22bb3d7c667693235515fb8b8080f15
sayed_hossain <sayed.hasan0011@gmail.com>
2023-12-03 21:04:31 +0600

    Post Contact request to database

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+    MongoClient,
+    ServerApiVersion
+} = require('mongodb');
 const port = process.env.PORT || 3000
 
 // Middleware 
 
-app.use(cors())
+app.use(cors('*'))
 app.use(express.json())
-const { ObjectId } = require('mongodb');
+const {
+    ObjectId
+} = require('mongodb');
 
 
 // Mongodb Code Start
@@ -20,145 +25,172 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const bioDataCollection = client.db('matrimoni-server').collection('bioData')
-    const requestCollection = client.db('matrimoni-server').collection('request')
-    const usersCollection = client.db('matrimoni-server').collection('users')
-
-    // Users CRUD Start 
-    app.get('/users', async(req, res) => {
-        const result = await usersCollection.find().toArray()
-        res.send(result)
-    })
-
-    app.post('/users', async(req, res) => {
-        const users = req.body
-        const result = await usersCollection.insertOne(users)
-        res.send(result)
-    })
+        const bioDataCollection = client.db('matrimoni-server').collection('bioData')
+        const requestCollection = client.db('matrimoni-server').collection('request')
+        const usersCollection = client.db('matrimoni-server').collection('users')
+        const contactCollection = client.db('matrimoni-server').collection('contact')
 
 
-    app.get('/users/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await usersCollection.findOne(query)
-        res.send(result)
-    })
+        // Contact CRUD START 
+        app.post('/contact', async(req, res) => {
+            const contacts = req.body
+            const result = await contactCollection.insertOne(contacts)
+            res.send(result)
+        })
+
+        // Contact CRUD End
+
+        // Users CRUD Start 
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const users = req.body
+            const result = await usersCollection.insertOne(users)
+            res.send(result)
+        })
 
 
-    app.delete('/users/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await usersCollection.deleteOne(query)
-        res.send(result)
-    })
-
-    // Make As Admin 
-    app.patch('/users/admin/:id', async(req, res) => {
-        const id = req.params.id
-        const filter = {_id: new ObjectId(id)}
-        const updateDoc = {
-            $set: {
-                role: 'admin'
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
             }
-        }
-        const result = await usersCollection.updateOne(filter, updateDoc)
-        res.send(result)
-    })
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
 
 
-    // Users CRUD End 
-
-
-
-
-    // Bio Data Collection Start
-    app.post('/bioData', async (req, res) => {
-        const bioData = req.body;
-        const result = await bioDataCollection.insertOne(bioData)
-        res.send(result)
-    })
-
-    app.get('/bioData', async (req, res) => {
-        const bioData = await bioDataCollection.find().toArray()
-        res.send(bioData)
-    })
-
-    app.delete('/bioData/:id', async (req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await bioDataCollection.deleteOne(query)
-        res.send(result)
-    })
-
-    app.get('/bioData/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await bioDataCollection.findOne(query)
-        res.send(result)
-    })
-    // Bio Data Collection End
-
-    // Request Form Start 
-    app.post('/request', async(req, res) => {
-        const request = req.body
-        const result = await requestCollection.insertOne(request)
-        res.send(result)
-    })
-
-    app.get('/request', async(req, res) => {
-        const request = req.body
-        const result = await requestCollection.find(request).toArray()
-        res.send(result)
-    })
-
-    app.get('/request/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const result = await requestCollection.findOne(query)
-        res.send(result)
-    })
-
-    app.delete('/request/:id', async(req, res) => {
-        const id = req.params.id
-        const query = {_id: new ObjectId(id)}
-        const deleteRequest = await requestCollection.deleteOne(query)
-        res.send(deleteRequest)
-    })
-
-    // Approve Request
-    app.patch('/request/approved/:id', async(req, res) => {
-        const id = req.params.id
-        const filter = {_id: new ObjectId(id)}
-        const updateDoc = {
-            $set: {
-                isApproved: true
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
             }
-        }
-        const result = await requestCollection.updateOne(filter, updateDoc)
-        res.send(result)
-    })
+            const result = await usersCollection.deleteOne(query)
+            res.send(result)
+        })
 
-    // Request Form End 
+        // Make As Admin 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+        // Users CRUD End 
+
+
+
+
+        // Bio Data Collection Start
+        app.post('/bioData', async (req, res) => {
+            const bioData = req.body;
+            const result = await bioDataCollection.insertOne(bioData)
+            res.send(result)
+        })
+
+        app.get('/bioData', async (req, res) => {
+            const bioData = await bioDataCollection.find().toArray()
+            res.send(bioData)
+        })
+
+        app.delete('/bioData/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await bioDataCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.get('/bioData/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await bioDataCollection.findOne(query)
+            res.send(result)
+        })
+        // Bio Data Collection End
+
+        // Request Form Start 
+        app.post('/request', async (req, res) => {
+            const request = req.body
+            const result = await requestCollection.insertOne(request)
+            res.send(result)
+        })
+
+        app.get('/request', async (req, res) => {
+            const request = req.body
+            const result = await requestCollection.find(request).toArray()
+            res.send(result)
+        })
+
+        app.get('/request/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await requestCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.delete('/request/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const deleteRequest = await requestCollection.deleteOne(query)
+            res.send(deleteRequest)
+        })
+
+        // Approve Request
+        app.patch('/request/approved/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const updateDoc = {
+                $set: {
+                    isApproved: true
+                }
+            }
+            const result = await requestCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        // Request Form End 
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -173,6 +205,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Matrimony is run on, ${port}`) 
+    console.log(`Matrimony is run on, ${port}`)
 })
-
